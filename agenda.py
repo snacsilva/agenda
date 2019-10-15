9c7671fbbe7a2333984dc17fca370a618b00e456
Samantha Silva <samanthasilva.n@gmail.com>
2019-10-15 19:50:49 -0300

    Buscar contato.

--- agenda.py
+++ agenda.py
@@ -1,19 +1,33 @@
 AGENDA = {}
 
 AGENDA['Carla'] = {
-    'tel': '99999999-99',
+    'tel': '99999-9999',
     'email': 'ohcarla@teste.com',
     'endereco': '123 Caçaba das aquarelas'
+}
+
+AGENDA['Sheyla'] = {
+    'tel': '90000-0099',
+    'email': 'loiradotcham@teste.com',
+    'endereco': 'Aguaraquaba, 2123'
 }
 
 
 def exibir_agenda():
     for contato in AGENDA:
         print('====================================')
-        print("Nome: ", contato )
-        print("Tel: ", AGENDA[contato]['tel'])
-        print("Email: ", AGENDA[contato]['email'])
-        print("Endereço: ", AGENDA[contato]['endereco'])
+        print('Nome: ', contato )
+        print('Tel: ', AGENDA[contato]['tel'])
+        print('Email: ', AGENDA[contato]['email'])
+        print('Endereço: ', AGENDA[contato]['endereco'])
 
 
-exibir_agenda()
+def buscar_contato(contato):
+    print('Nome: ', contato )
+    print('Tel: ', AGENDA[contato]['tel'])
+    print('Email: ', AGENDA[contato]['email'])
+    print('Endereço: ', AGENDA[contato]['endereco'])
+
+
+# exibir_agenda()
+buscar_contato('Sheyla')
